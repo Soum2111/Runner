05e858eb821c2040a84fcf218a4337cf4d452148
Soum <soumaya.metrab@gmail.com>
2022-10-25 10:37:01 +0200

    Initial commit

---
+++ src/Camera.java
@@ -0,0 +1,26 @@
+public class Camera {
+    int x;
+    int y;
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "Camera{" +
+                "x=" + x +
+                '}';
+    }
+
+
+    public String toStringy() {
+        return "Camera{" +
+                "y=" + y +
+                '}';
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+        @Override
+        public void start(Stage primaryStage) throws Exception{
+
+            primaryStage.setTitle("Hello world");
+            Group root = new Group();
+            Pane pane = new Pane(root);
+            Scene theScene = new Scene(pane, 600, 400,true);
+
+
+            Image spriteSheet = new Image("file:Image/desert.png");
+            ImageView sprite = new ImageView(spriteSheet);
+            sprite.setViewport(new Rectangle2D(20,0,65,100));
+            sprite.setX(200);
+            sprite.setY(300);
+            primaryStage.setScene(theScene);
+            root.getChildren().add(sprite);
+            primaryStage.show();
+
+        }
+
+
+        public static void main(String[] args) {
+        launch(args);
+        }
+}
